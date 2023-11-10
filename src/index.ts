4ce46a188bc0d03b5b26ae27e8b94597aed0fd83
Andrea Baretta <andrea.m.baretta@gmail.com>
2023-11-10 14:02:39 -0800

    Add logging middleware

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,25 @@
 import express, { Application, Request, Response } from "express";
 import bodyParser from "body-parser";
 import mongoose, { Promise } from "mongoose";
+import { User, IUser, Preferences, IPreferences } from "./mongoSchema";
+import { morganMiddleware } from "./utils/logger";
 
 const uri = 'mongodb://tester:' + encodeURIComponent("KwO9?$/q@HhZEf?PPzwM") + '@127.0.0.1:27017/test?authSource=admin';
-mongoose.connect(uri);
-import { User, IUser, Preferences, IPreferences } from "./mongoSchema";
-
-
-const newUser = new User({name : "New", surname: "User", username: "hei", preferences: {universityHousing: false}})
-
-//User.insertMany(newUser);
-//User.updateOne({name: "Lee"}, {name: "Lu"} ).exec().then((result)=>{console.log("Updated")});
-//const doc = User.find({username: 'hei'});
-
-
-
-User.find().exec().then(doc => {
-  console.log("World");
-  console.log(doc.toString());
-});//example of using mongo
-//is .then() the same as await()?
-
-
+mongoose.connect(uri).then(
+  () => {
+      console.log("Successfully connected to mongodb serever!");
+  },
+).catch(err => {
+  console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
+  // process.exit();
+});
 
 const app: Application = express();
 app.use(express.json());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morganMiddleware);
 
 app.get("/users", (req: Request, res: Response) => {
   //res.send("Healthy");
