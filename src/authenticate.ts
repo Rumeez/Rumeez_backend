97b2210da2aef2fd825d25d52b9bead911c6b954
Andrea Baretta <andrea.m.baretta@gmail.com>
2023-11-11 22:55:41 -0800

    Add code to read jwt payload

--- src/authenticate.ts
+++ src/authenticate.ts
@@ -4,6 +4,8 @@ import User from './models/user.interface';
 import jwt from 'jsonwebtoken';
 import passportJwt, { ExtractJwt, VerifiedCallback, VerifyCallback } from 'passport-jwt';
 import { config } from './config';
+import ResponseError from './ResponseError';
+import { Request } from 'express';
 
 const opts: passportJwt.StrategyOptions = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -30,3 +32,79 @@ export const getToken: GetToken = function(user: any): string {
         expiresIn: 3600 //Expires in one hour
     });
 };
+
+export interface FullJWT {
+    token: {
+        header: {
+            alg: string,
+            typ: string
+        };
+        payload: {
+            email: string,
+        };
+        signature: string;
+    };
+    err: ResponseError | null;
+}
+
+const EmptyToken: FullJWT["token"] = {
+        header: {
+            alg: "",
+            typ: ""
+        },
+        payload: {
+            email: "",
+        },
+        signature: ""
+}
+
+function isFullJWT(obj: any): boolean {
+    try {
+        const objFullJWT: FullJWT = obj;
+        return true;
+    } catch {
+        return false;
+    }
+}
+
+
+export const jwtFromHeader = function(req: Request): FullJWT {
+    const token: string | undefined = req.headers.authorization;
+    if (token) {
+        console.log("Before verify");
+        try {
+            console.log("token: " + token.split(' ')[1]);
+            const decryptedToken: string | object = jwt.verify(token.split(' ')[1], config.secretKey, {complete: true});
+            console.log("Made it past verify");
+            if (typeof decryptedToken === "object") {
+                console.log("Obj");
+                console.log("decryptedToken: " + JSON.stringify(decryptedToken));
+                const fullJWTcandidate: any = {token: decryptedToken, err: null}
+                if (isFullJWT(fullJWTcandidate)) {
+                    console.log("Is FullJWT");
+                    return fullJWTcandidate;
+                } else {
+                    console.log("Isn't FullJWT");
+                    const err: ResponseError = new Error("Bad JWT Token");
+                    err.status = 400;
+                    return {token: EmptyToken, err: err};
+                }
+            } else {
+                console.log("String");
+                console.log("decryptedToken: " + decryptedToken);
+                const err: ResponseError = new Error("Bad JWT Token");
+                err.status = 400;
+                return {token: EmptyToken, err: err};
+            }
+        } catch {
+            console.log("No bueno")
+            const err: ResponseError = new Error("Invalid JWT");
+            err.status = 403;
+            return {token: EmptyToken, err: err};
+        }
+    } else {
+        const err: ResponseError = new Error("Bad JWT Token");
+        err.status = 400;
+        return {token: EmptyToken, err: err};
+    }
+}
--- src/routers/usersRouter.ts
+++ src/routers/usersRouter.ts
@@ -5,25 +5,32 @@ import bcrypt from 'bcrypt';
 import { getToken } from '../authenticate';
 import mongoose from 'mongoose';
 import ResponseError from '../ResponseError';
+import { FullJWT, jwtFromHeader } from '../authenticate';
 
 const usersRouter = express.Router();
 
 usersRouter.route('/')
     .get(passport.authenticate('jwt', {session: false}), function (req: Request, res: Response, next: NextFunction): void {
-        console.log("Inside GET")
-        userModel.find({})
-            .then(function (users: any[]) {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(users);
-                console.log("Successfully found users");
-            }, function (err: ResponseError) { next(err) })
-            .catch(function (err: ResponseError) { next(err) });
+        console.log("Inside GET");
+        const validate: FullJWT = jwtFromHeader(req);
+        if (validate.err) {
+            next(validate.err);
+        } else {
+            userModel.find({email: validate.token.payload.email})
+                .then(function (users: any[]) {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(users);
+                    console.log("Successfully found users");
+                }, function (err: ResponseError) { next(err) })
+                .catch(function (err: ResponseError) { next(err) });
+        }
     });
 
 usersRouter.route('/login')
     .post((req: Request, res: Response, next: NextFunction): void => {
         const userData = req.body;
+        console.log("Performing login");
         userModel.findOne({ "email": userData.email })
         .then((user): void => {
             if (user === null) {
@@ -32,8 +39,11 @@ usersRouter.route('/login')
                 res.setHeader('Content-Type', 'text/plain');
                 res.send("Wrong email");
             } else {
+                const t1 = new Date().getTime();
                 bcrypt.compare(userData.password, user.toJSON().password)
                 .then((same: Boolean) => {
+                    const t2 = new Date().getTime();
+                    console.log("bcrypt compare took: " + (t2-t1) + "ms");
                     if (same) {
                         const token = getToken(user.toJSON());
                         res.statusCode = 200;
